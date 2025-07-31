bff45d691fe4881e2d8c88350db78dd8a1c45769
Ahmetnasri <ahmet.nasri@outlook.com>
2025-07-31 17:57:25 +0200

    CloudFront distribution added

--- test.tf
+++ test.tf
@@ -40,6 +40,56 @@ resource "aws_security_group" "ssh_access" {
   }
 }
 
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_instance.web_test.public_dns 
+    origin_id   = "EC2Origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "EC2Origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+
+output "cloudfront_url" {
+  description = "CloudFront Distribution Domain Name"
+  value       = aws_cloudfront_distribution.cdn.domain_name
+}
+
+
 output "instance_public_ip" {
   value = aws_instance.web_test.public_ip
 }
