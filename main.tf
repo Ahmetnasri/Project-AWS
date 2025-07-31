61a570856b23b9d06eba9ecfeda8432d7cf79235
Ahmetnasri <ahmet.nasri@outlook.com>
2025-07-31 18:00:07 +0200

    CloudFront distribution added with S3

--- main.tf
+++ main.tf
@@ -82,3 +82,52 @@ output "index_html_url" {
   description = "Direct HTTPS URL to index.html"
   value       = "https://${aws_s3_bucket.s3bucket.bucket}.s3.${var.aws_region}.amazonaws.com/index.html"
 }
+
+
+resource "aws_cloudfront_distribution" "cdn" {
+  origin {
+    domain_name = aws_s3_bucket.s3bucket.bucket_regional_domain_name
+    origin_id   = "s3-origin"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
+
+  enabled             = true
+  default_root_object = "index.html"
+
+  default_cache_behavior {
+    allowed_methods  = ["GET", "HEAD"]
+    cached_methods   = ["GET", "HEAD"]
+    target_origin_id = "s3-origin"
+
+    forwarded_values {
+      query_string = false
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
+  viewer_certificate {
+    cloudfront_default_certificate = true
+  }
+}
+
+
+output "cloudfront_url" {
+  description = "CloudFront Distribution Domain Name"
+  value       = aws_cloudfront_distribution.cdn.domain_name
+}
