ce3426ce4677cbccfdb4bd24428db56291c0f8fe
Ahmetnasri <ahmet.nasri@outlook.com>
2025-09-14 23:45:11 +0200

    Variables file added

--- variables.tf
+++ variables.tf
@@ -6,6 +6,12 @@ variable "aws_region" {
 
 # ami of ec2
 variable "ec2_ami" {
-  description = "Name of the S3 bucket"
+  description = "AMI ID of the EC2"
   default = "ami-0af9b40b1a16fe700"
+}
+
+# EC2 Instance Type
+variable "instance_type" {
+  description = "Type of the EC2 Instance"
+  default = "t2.micro"
 }
